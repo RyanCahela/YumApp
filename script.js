80bfdb6da5aa3649aebbf3f3f5332cfe4e03c378
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-03-02 13:21:34 -0800

    event listenter

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log('js connected');
 
+function startSearch(){
+  $('.submitbtn').on('submit', function(event){
+    event.preventDefault();
+    let searchCity = $('#city-input').val();
+    let searchState = $('#state-select').val();
+    let searchZip = $('#zipcode-input').val();
+    let searchType= $('.type').val();
+    
+  });
+
+}
+
+
+
