a72934d58735a874449beb43eb7375bac2ec3ecb
Ryan Cahela <ryancahela@gmail.com>
2019-03-02 17:25:14 -0800

    added proxy to fix cors issues temporarily

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let locationType = "brewpubs"
 
 
 function formatQuery(locationType,...args) {
-    let baseEndpoint = "https://api.yelp.com/v3/businesses/search"
+    let baseEndpoint = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search"
     let filteredArgs = args.filter((item) => {if (item !== "") {return item}});
     let searchQuery = encodeURIComponent(filteredArgs.join(' '));
     let formattedQuery = `${baseEndpoint}?location=${searchQuery}&categories=${locationType}`;  
@@ -16,11 +16,20 @@ function formatQuery(locationType,...args) {
     getLocations(formattedQuery);
 }
 
+var myHeaders = new Headers();
+myHeaders.append("Authorization","Bearer xIv0uKoLBvYlWMNctGn5vkjf8dcz-Qi0OqTFSMtOmK8GXiw8atmJIbpNbEyAwd2CGeESdgh-RHOs12SySQUiZlIl3Ilv7Cy2F_APfJu07wnZELHuTXyXscSAzed6XHYx")
+
+console.log(myHeaders);
+console.log(myHeaders.get('Authorization'));
+
+let options = {
+    headers: myHeaders
+};
+
+console.log(options);
 
 function getLocations(seachQuery) {
-    fetch(seachQuery,{
-        Bearer: "xIv0uKoLBvYlWMNctGn5vkjf8dcz-Qi0OqTFSMtOmK8GXiw8atmJIbpNbEyAwd2CGeESdgh-RHOs12SySQUiZlIl3Ilv7Cy2F_APfJu07wnZELHuTXyXscSAzed6XHYx"
-    })
+    fetch(seachQuery,options)
     .then(function(response) {return response.json()})
     .then(function(responseJson) {displayResults(responseJson)})
     .catch(error => {throw 'something went worng'});
