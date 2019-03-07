c0b6a62ea70d42d3e99bc7879af927d113edf4a8
Ryan Cahela <ryancahela@gmail.com>
2019-03-06 17:22:25 -0800

    added Form,Format,Fetch,Display objects

--- script.js
+++ script.js
@@ -1,104 +1,166 @@
 console.log('js connected');
 
-function startSearch(){
-  $('.form').on('submit', function(event){
-    event.preventDefault();
-    let searchCity = $('#city-input').val();
-    let searchState = $('#state-select').val();
-    let searchZip = $('#zipcode-input').val();
-    let locationType = $('.location-type').val();
+document.addEventListener("DOMContentLoaded", function onDOMLoad() {
+  console.log("DOM Loaded");
 
-    let formattedQuery = formatQuery(locationType,searchCity,searchState,searchZip);
-    let resultsJson = getLocations(formattedQuery);
-    console.log('startSearch ran');
-  });
-}
+  const App = initializeApp();
 
-function formatQuery(locationType,...args) {
-    let baseEndpoint = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken%20breast"
-    let filteredArgs = args.filter((item) => {if (item !== "") {return item}});
-    let searchQuery = encodeURIComponent(filteredArgs.join(' '));
-    let formattedQuery = `${baseEndpoint}?location=${searchQuery}&categories=${locationType}`;
-    console.log(formattedQuery);  
-    return formattedQuery;
-}
+  App.form.element.addEventListener("submit", function(e) {
+    e.preventDefault();
+    App.form.setValuesFromForm();
+    let searchTermObj = App.form.getValues();
 
-function getLocations(seachQuery) {
-    let header = new Headers();
-    header.append("Authorization", "Bearer xIv0uKoLBvYlWMNctGn5vkjf8dcz-Qi0OqTFSMtOmK8GXiw8atmJIbpNbEyAwd2CGeESdgh-RHOs12SySQUiZlIl3Ilv7Cy2F_APfJu07wnZELHuTXyXscSAzed6XHYx");
-    header.append("mode", "no-cors");
-    let options = {
-      headers: header
-    };
+    console.log(searchTermObj);
+    App.searchMeals.buildUrl(null,searchTermObj);
+    let fetchResolved = App.searchMeals.fetch(whenMealFetchResolves);
+    function whenMealFetchResolves() {
+      App.listFormatter.setJson(App.searchMeals.json);
+      App.listFormatter.formatData();
+      let result = App.listFormatter.getFormattedData();
+      console.log(result);
+      App.displayResultsList(result);
+    }
+  })
+});
 
-    fetch(seachQuery,options)
-    .then(function(response) {
-      if(response.ok){
-        return response.json();
+
+function initializeApp() {
+  function Form(formElement) {
+    this.element = formElement;
+    this.queries = {
+      mainIngredientQuery: null,
+      categoryQuery: null,
+      cuisineQuery: null
+    }
+  }
+  const formFunctions = {
+    setValuesFromForm: function() {
+      function assignValueToProp(nameOfProp, CSSselectorOfElement) {
+        this.queries[nameOfProp] = document.querySelector(CSSselectorOfElement).value;
+      };
+      assignValueToProp.call(this,"mainIngredientQuery", "#ingredient-input");
+      assignValueToProp.call(this,"categoryQuery", "#category-select");
+      assignValueToProp.call(this,"cuisineQuery", "#country-of-origin-select");
+    },
+    getValues: function() {
+      function validateFormInputs(queriesObj) {
+        let valueWasEntered = false;
+        for(prop in queriesObj) {
+          if(queriesObj[prop]) {
+            valueWasEntered = true;
+          }
+        }
+        return valueWasEntered;
       }
-      throw 'response not 200';      
-    })
-    .then(function(responseJson) {
-      console.log(responseJson);
-      displayResults(responseJson);
-    })
-    .catch(error => new Error(error));
+      let valueWasEntered = validateFormInputs(this.queries);
+      if(valueWasEntered) {
+        return this.queries;
+      } else {
+        throw "You must enter something to search";
+      }
+    }
+    }
+
+  function Fetch(baseEndpoint,headers) {
+    this.url = baseEndpoint;
+    this.headers = headers;
+    this.json = null;
+    this.isResolved = false;
+  }
+  const fetchFunctions = {
+    fetch: function(callback) {
+      console.log(this.url);
+      fetch(this.url,this.headers)
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+        throw "something went wrong";
+      })
+      .then(responseJson => {
+        this.setJson(responseJson);
+        callback();
+      })
+      .catch(err => new Error(err));
+    },
+    getJson: function() {
+      return this.json;
+    },
+    setJson: function(json) {
+      this.json = json;
+    },
+    buildUrl: function(mealId,termObj) {
+      let searchTerms = "";
+      console.log(!mealId);  
+      if(!mealId) {
+        console.log("mealId is null"); 
+        let termArray = [];
+        if(termObj.mainIngredientQuery){
+          termArray.push(`i=${termObj.mainIngredientQuery}`);
+        }
+        if(termObj.categoryQuery){
+          termArray.push(`c=${termObj.categoryQuery}`);
+        }
+        if(termObj.cuisineQuery){
+          termArray.push(`a=${termObj.cuisineQuery}`);
+        }
+        searchTerms = termArray.join("&");
+        this.url += searchTerms;
+        console.log(searchTerms);
+      } else {
+        searchTerms = mealId;
+      this.url += searchTerms;
+      }
+    },
+    getUrl: function() {
+      return this.url;
+    }
+  }
+  function FormatList() {
+    this.data = null;
+    this.formattedOutput = "";
+  }
+  const formatListFunctions = {
+    formatData: function() {
+      let stringArray = this.data.meals.map(function(item) { 
+        return `<li class="result">
+        <h3 class="result__heading">${item.strMeal}</h3>
+        <a class="result__link" href="#">
+          <img class="result__link__image" src=${item.strMealThumb} alt=${item.strMeal}>
+        </a>      
+      </li>`;
+      });
+      this.formattedOutput = stringArray.join(' ');
+    },
+    getFormattedData: function() {
+      return this.formattedOutput;
+    },
+    getData: function() {
+      return this.data;
+    },
+    setJson: function(json) {
+      this.data = json;
+    }
+
+  }
+
+
+  let myForm = new Form(document.querySelector(".form"));
+  Object.setPrototypeOf(myForm, formFunctions);
+
+  let myFetch = new Fetch("https://www.themealdb.com/api/json/v1/1/filter.php?",{});
+  Object.setPrototypeOf(myFetch, fetchFunctions);
+
+  let myFormatter = new FormatList();
+  Object.setPrototypeOf(myFormatter,formatListFunctions);
+
+  return {
+    form: myForm,
+    searchMeals: myFetch,
+    listFormatter: myFormatter,
+    displayResultsList: function(string) {
+      let resultsList = document.getElementById("js-results-list");
+      resultsList.innerHTML = string;
+    }   
+  }
 }
-
-function displayResults(responseJson) {
-  let $resultsUl = $(".results-list");
-
-  let businessArray = responseJson.businesses;
-  
-
-  let elementArray = businessArray.map((item) => {
-
-    let resultInfoObj = {
-      name: item.name,
-      address: item.location.display_address.join(' '),
-      phone: item.display_phone,
-      imageUrl: item.image_url,
-      webUrl: item.url
-    };
-
-
-    let element = createResultItem(resultInfoObj);
-    return element;
-  }); 
-
-  $resultsUl.html(elementArray);
-}
-
-function createResultItem(resultInfoObj) {
-
-
-  let element = `<li class="result-item">
-  <ul class="result-item-list"> 
-    <li class="result-item__heading">${resultInfoObj.name}</li>
-    <li class="result-item__address">${resultInfoObj.address}</li>
-    <li class="result-item__phone">${resultInfoObj.phone}</li>
-    <li>
-    <a href=${resultInfoObj.webUrl}>
-      <img src=${resultInfoObj.imageUrl}>
-    </a>
-    </li>
-    <li>
-      <a class="result-item__link" href=${resultInfoObj.webUrl}>View Yelp Page</a>
-    </li>
-  </ul>
-  </li>`;
-
-  return element;
-
-};
-
-
-
-$(startSearch());
-
-
-fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken%20breast')
-.then(response => response.json())
-.then(responseJson => console.log(responseJson))
-.catch(error => new Error(error));
-
