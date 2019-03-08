98ad6aa014aace060da79fcfc6c6fa92b9d856f0
Ryan Cahela <ryancahela@gmail.com>
2019-03-07 21:58:20 -0800

    search and display results fully functioning, menu buttons work

--- script.js
+++ script.js
@@ -12,220 +12,209 @@ document.addEventListener("DOMContentLoaded", function onDOMLoad() {
     document.dispatchEvent(event);
   }
 
-  const App = initializeApp();
-
+  //manages App state and switching of views
   document.addEventListener("stateChanged", function(e) {
-    if(e.detail === "search") {
-      
+    console.log("change State ran with value " + e.detail.state);
+    if(e.detail.state === "search") {
+      App.searchViewManager.render(App.searchViewManager.html);
+      const form = document.getElementById("js-form");
+      form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        App.searchViewManager.getValuesFromForm();  
+        if(App.searchViewManager.formInputsValid()) {
+            //make fetch
+            let url = App.searchViewManager.fetch.buildMealSearchUrl(App.searchViewManager.queries);
+            App.searchViewManager.fetch.getData(url,null,afterListFetch);
+        } else {
+            App.searchViewManager.displaySearchErrorToUser();
+        }   
+        //prevents execution until list json is available
+        function afterListFetch(jsonData) {
+          App.resultManager.setData.call(App.resultManager,jsonData);
+          App.resultManager.formatResultData.call(App.resultManager);
+          changeState("results");
+        }
+      });
+    } else {}
+    if(e.detail.state === "results") {
+      App.resultManager.render(App.resultManager.html);
+      const resultsList = document.getElementById('result-list');
+      resultsList.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log(e.target);
+      })
     }
   });
-  
 
-  
-  App.searchViewManager.render(App.searchViewManager.html);
-  App.state = "search";
+  function initializeApp() {
+    const ManagerFunctions = {
+      render: function(html) {
+        let element = document.getElementById("body");
+        element.innerHTML = html;
 
-  let searchState = new Promise((resolve,reject) => {
-    const form = document.getElementById("js-form");
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      App.searchViewManager.getValuesFromForm();  
-      if(App.searchViewManager.formInputsValid()) {
-          //make fetch
-          let url = App.searchViewManager.fetch.buildMealSearchUrl(App.searchViewManager.queries);
-          App.searchViewManager.fetch.getData(url,null,afterListFetch);
-      } else {
-          App.searchViewManager.displaySearchErrorToUser();
-      }
-      
-      //prevents execution until list json is available
-      function afterListFetch(jsonData) {
-        App.resultManager.setData.call(App.resultManager,jsonData);
-        App.resultManager.formatResultData.call(App.resultManager);
-        App.resultManager.render.call(App.resultManager,App.resultManager.html);
-        resolve(()=>{App.state = "result"});
-      }
+        //must reset menu listeners on render
+        function addMenuButtonListeners() {
+          let button = document.querySelector("#js-button-li");
+          let linksLi = document.querySelector("#js-link-li");
+          let searchBtn = document.querySelector("#js-menu-search-button");
+          let resultsBtn = document.querySelector("#js-menu-results-button");
+          button.addEventListener("click", function() {
+            linksLi.classList.toggle("hidden");
+          });
 
-    });
-  }).then(()=>{
-    App.state = "result";
-    console.log(App.state);
-
-    const resultList = document.getElementById("result-list");
-    resultList.addEventListener("click", function(e) {
-      e.preventDefault();
-      console.log(e.target);
-    });
-  });
-});
-
-
-function initializeApp() {
-  const ManagerFunctions = {
-    render: function(html) {
-      let element = document.getElementById("body");
-      element.innerHTML = html;
-
-      //must reset menu listeners on render
-      function addMenuButtonListeners() {
-        let button = document.querySelector("#js-button-li");
-        let linksLi = document.querySelector("#js-link-li");
-        button.addEventListener("click", function() {
-          linksLi.classList.toggle("hidden");
-        });
-      }
-      addMenuButtonListeners();
-    },
-    fetch: {
-      buildMealSearchUrl:function(termObj) {
-        //formats the url to be compatable with themealdb api
-        let baseEndpoint = "https://www.themealdb.com/api/json/v1/1/filter.php?";
-        let termArray = [];
-        if(termObj.mainIngredientQuery){
-          termArray.push(`i=${termObj.mainIngredientQuery}`);
-        }
-        if(termObj.categoryQuery){
-          termArray.push(`c=${termObj.categoryQuery}`);
-        }
-        if(termObj.cuisineQuery){
-          termArray.push(`a=${termObj.cuisineQuery}`);
-        }
-        return baseEndpoint + termArray.join("&");
-      },
-      buildDetailSearchUrl: function(mealId) {
-        let baseEndpoint = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
-        return baseEndpoint + String(mealId);
-      },
-      getData: function(url,headers,callback) {
-        fetch(url,headers)
-          .then(response => {
-            if(response.ok) {
-              return response.json();
-            }
-            throw "something went wrong";
+          searchBtn.addEventListener("click", function() {
+            changeState("search");
+          });
+          resultsBtn.addEventListener("click", function() {
+            changeState("results");
           })
-          .then(responseJson => {
-            if(callback) {
-            callback(responseJson);
-            return;
-            }
-            return responseJson;
-          })
-          .catch(err => new Error(err));
         }
-      }
-  }
-  function SearchManager() {
-    this.html = templates.searchTemplate;
-    this.queries = {
-      mainIngredientQuery: null,
-      categoryQuery: null,
-      cuisineQuery: null
-    },
-      //Event Listener Helper Functions
-    this.formInputsValid = function() {
-        let queriesObj = this.queries;
-        let valueWasEntered = false;
-        for(prop in queriesObj) {
-          if(queriesObj[prop]) {
-            valueWasEntered = true;
+        addMenuButtonListeners();
+      },
+      fetch: {
+        buildMealSearchUrl:function(termObj) {
+          //formats the url to be compatable with themealdb api
+          let baseEndpoint = "https://www.themealdb.com/api/json/v1/1/filter.php?";
+          let termArray = [];
+          if(termObj.mainIngredientQuery){
+            termArray.push(`i=${termObj.mainIngredientQuery}`);
+          }
+          if(termObj.categoryQuery){
+            termArray.push(`c=${termObj.categoryQuery}`);
+          }
+          if(termObj.cuisineQuery){
+            termArray.push(`a=${termObj.cuisineQuery}`);
+          }
+          return baseEndpoint + termArray.join("&");
+        },
+        buildDetailSearchUrl: function(mealId) {
+          let baseEndpoint = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+          return baseEndpoint + String(mealId);
+        },
+        getData: function(url,headers,callback) {
+          fetch(url,headers)
+            .then(response => {
+              if(response.ok) {
+                return response.json();
+              }
+              throw "something went wrong";
+            })
+            .then(responseJson => {
+              if(callback) {
+              callback(responseJson);
+              return;
+              }
+              return responseJson;
+            })
+            .catch(err => new Error(err));
           }
         }
-        return valueWasEntered;
-    },
-    this.displaySearchErrorToUser = function() {
-      let form = document.querySelector("#js-form");
-      //so only one error message is displayed upon multiple clicks.
-      if (form.children[0].classList.contains('error-message')) {
-        return;
+    }
+    function SearchManager() {
+      this.html = templates.searchTemplate;
+      this.queries = {
+        mainIngredientQuery: null,
+        categoryQuery: null,
+        cuisineQuery: null
+      },
+        //Event Listener Helper Functions
+      this.formInputsValid = function() {
+          let queriesObj = this.queries;
+          let valueWasEntered = false;
+          for(prop in queriesObj) {
+            if(queriesObj[prop]) {
+              valueWasEntered = true;
+            }
+          }
+          return valueWasEntered;
+      },
+      this.displaySearchErrorToUser = function() {
+        let form = document.querySelector("#js-form");
+        //so only one error message is displayed upon multiple clicks.
+        if (form.children[0].classList.contains('error-message')) {
+          return;
+        };
+        let errorMessage = document.createElement("div");
+        errorMessage.classList.add("error-message");
+        errorMessage.textContent = "Please select from the dropdowns or search by main ingredient";
+        form.prepend(errorMessage);
+      },
+      this.getValuesFromForm = function() {
+        function assignValueToProp(nameOfProp, CSSselectorOfElement) {
+          this.queries[nameOfProp] = document.querySelector(CSSselectorOfElement).value;
+        };
+        assignValueToProp.call(this,"mainIngredientQuery", "#ingredient-input");
+        assignValueToProp.call(this,"categoryQuery", "#category-select");
+        assignValueToProp.call(this,"cuisineQuery", "#country-of-origin-select");
+      }
+      
+    }
+    function ResultManager() {
+      this.data = null;
+      this.html = templates.resultTemplate;
+      this.listObjects = [];
+      this.resultList = "";
+      this.formatResultData = function() {
+        let stringArray = this.data.map(function(item) { 
+          return `<li class="result">
+          <h3 class="result__heading">${item.strMeal}</h3>
+          <a class="result__link" href="#">
+            <img class="result__link__image" src=${item.strMealThumb} alt=${item.strMeal}>
+          </a>      
+        </li>`;
+        });
+
+
+        let div = document.createElement("div");
+        let ul = document.createElement("ul");
+
+        div.classList.add("results-container");
+        ul.classList.add("result-list");
+        ul.id = "result-list"
+        div.appendChild(ul);
+        ul.innerHTML = stringArray.join(' ');
+
+        this.html += div.innerHTML;
       };
-      let errorMessage = document.createElement("div");
-      errorMessage.classList.add("error-message");
-      errorMessage.textContent = "Please select from the dropdowns or search by main ingredient";
-      form.prepend(errorMessage);
-    },
-    this.getValuesFromForm = function() {
-      function assignValueToProp(nameOfProp, CSSselectorOfElement) {
-        this.queries[nameOfProp] = document.querySelector(CSSselectorOfElement).value;
+      this.insert
+      this.getFormattedData = function() {
+        return this.formattedOutput;
       };
-      assignValueToProp.call(this,"mainIngredientQuery", "#ingredient-input");
-      assignValueToProp.call(this,"categoryQuery", "#category-select");
-      assignValueToProp.call(this,"cuisineQuery", "#country-of-origin-select");
+      this.getData = function() {
+        return this.data;
+      };
+      this.setData = function(json) {
+        console.log("setData ran");
+        this.data = json.meals;
+      }
     }
-    
-  }
+    function MenuButton(buttonSelector, linkSelector) {
+      this.button = document.querySelector(buttonSelector);
+      this.linksLi = document.querySelector(linkSelector);
+      this.toggleExpanded = function() {
+        this.linksLi.classList.toggle("hidden");  
+      }
+    }
 
-  function ResultManager() {
-    this.data = null;
-    this.html = templates.resultTemplate;
-    this.listObjects = [];
-    this.resultList = "";
-    this.formatResultData = function() {
-      let stringArray = this.data.map(function(item) { 
-        return `<li class="result">
-        <h3 class="result__heading">${item.strMeal}</h3>
-        <a class="result__link" href="#">
-          <img class="result__link__image" src=${item.strMealThumb} alt=${item.strMeal}>
-        </a>      
-      </li>`;
-      });
+    let searchViewManager = new SearchManager();
+    let resultManager = new ResultManager();
+    let menuButton = new MenuButton("#js-button-li","#js-link-li");
+
+    Object.setPrototypeOf(resultManager, ManagerFunctions);
+    Object.setPrototypeOf(searchViewManager, ManagerFunctions);
 
 
-      let div = document.createElement("div");
-      let ul = document.createElement("ul");
+    console.log(resultManager.__proto__);
 
-      div.classList.add("results-container");
-      ul.classList.add("result-list");
-      ul.id = "result-list"
-      div.appendChild(ul);
-      ul.innerHTML = stringArray.join(' ');
-
-      this.html += div.innerHTML;
-    };
-    this.insert
-    this.getFormattedData = function() {
-      return this.formattedOutput;
-    };
-    this.getData = function() {
-      return this.data;
-    };
-    this.setData = function(json) {
-      console.log("setData ran");
-      this.data = json.meals;
+    return {
+      searchViewManager: searchViewManager,
+      menuButton: menuButton,
+      resultManager: resultManager
     }
   }
 
-  function MenuButton(buttonSelector, linkSelector) {
-    this.button = document.querySelector(buttonSelector);
-    this.linksLi = document.querySelector(linkSelector);
-    this.toggleExpanded = function() {
-      this.linksLi.classList.toggle("hidden");  
-    }
-  }
-
-
-  let searchViewManager = new SearchManager();
-
-  let resultManager = new ResultManager();
-  
-  let menuButton = new MenuButton("#js-button-li","#js-link-li");
-
-  Object.setPrototypeOf(resultManager, ManagerFunctions);
-  Object.setPrototypeOf(searchViewManager, ManagerFunctions);
-
-
-  console.log(resultManager.__proto__);
-
-  return {
-    searchViewManager: searchViewManager,
-    menuButton: menuButton,
-    resultManager: resultManager
-  }
-}
-
-
-
-const templates = (function() {
+  //templates keep collapsed unless you need
+  var templates = (function() {
   const searchTemplate = `
   <header class="header" role="banner">
   <nav class="nav" role="navigation">
@@ -239,8 +228,8 @@ const templates = (function() {
     </ul>
     <h2 class="nav__subheading">The world's best recipe search engine</h2>
     <div class="mobile-nav hidden" id="js-link-li">
-        <a class="mobile-nav-link" href="#">Search</a>
-        <a class="mobile-nav-link" href="#">Results</a>
+        <a class="mobile-nav-link" href="#" id="js-menu-search-button">Search</a>
+        <a class="mobile-nav-link" href="#" id="js-menu-results-button">Results</a>
     </div>
   </nav>
   </header>
@@ -316,8 +305,8 @@ const templates = (function() {
     </ul>
     <h2 class="nav__subheading">The world's best recipe search engine</h2>
     <div class="mobile-nav hidden" id="js-link-li">
-        <a class="mobile-nav-link" href="#">Search</a>
-        <a class="mobile-nav-link" href="#">Results</a>
+        <a class="mobile-nav-link" href="#" id="js-menu-search-button">Search</a>
+        <a class="mobile-nav-link" href="#" id="js-menu-results-button">Results</a>
     </div>
   </nav>
   </header>
@@ -327,4 +316,8 @@ return {
   searchTemplate: searchTemplate,
   resultTemplate: resultTemplate
 }
-})();
+  })();
+  const App = initializeApp();
+  changeState("search");
+
+});
